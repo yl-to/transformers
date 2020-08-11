de0d678af783ccee75bde2d8230057e01a28e0df
yl-to <liu.yu5@outlook.com>
2020-08-10 23:35:33 -0700

    more

--- src/transformers/data/datasets/language_modeling.py
+++ src/transformers/data/datasets/language_modeling.py
@@ -121,18 +121,23 @@ class LineByLineWithSOPTextDataset(Dataset):
         import psutil
         start_time = time.time()
         for subfolder in os.listdir(file_dir):
+            print('probe1')
             for file_name in os.listdir(os.path.join(file_dir, subfolder)):
+                print('probe2')
                 file_start_time = time.time()
                 file_path = os.path.join(file_dir, subfolder, file_name)
                 assert os.path.isfile(file_path)
                 article_open = False
                 with open(file_path, encoding="utf-8") as f:
+                    print('probe3')
                     original_lines = f.readlines()
                     article_lines = []
                     for line in original_lines:
+                        print('probe4')
                         if '<doc id=' in line:
                             article_open = True
                         elif '</doc>' in line:
+                            print('probe5')
                             article_open = False
                             conversion_start_time = time.time()
                             document = [tokenizer.convert_tokens_to_ids(tokenizer.tokenize(line)) 
@@ -146,13 +151,18 @@ class LineByLineWithSOPTextDataset(Dataset):
                         else:
                             if article_open:
                                 article_lines.append(line)
+                    print('probe6')
+                print('probe7')
                 file_end_time = time.time()
                 count += 1
+                print('------------------------------------------------------------------')
                 print(f"single file parse time {round(file_end_time - file_start_time)}")
                 print(f"token conversion time {round(conversion_end_time - conversion_start_time)}")
                 print(f"examples generation time {round(example_start - example_end)}")
                 print(f"virtual_memory usage {psutil.virtual_memory().percent}")
                 print(f"files finished {count}/{all_file_count}")
+                print('------------------------------------------------------------------')
+            print('probe8')
 
         logger.info(f"Dataset parse finished.")
 
