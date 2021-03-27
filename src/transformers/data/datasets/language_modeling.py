5be7a93e2bab96001d1639db41ce23f33ea572dc
yu <liu.yu5@outlook.com>
2021-03-27 01:07:16 +0000

    modified for multi-file parsing

--- src/transformers/data/datasets/language_modeling.py
+++ src/transformers/data/datasets/language_modeling.py
@@ -116,25 +116,30 @@ class LineByLineTextDataset(Dataset):
     This will be superseded by a framework-agnostic approach soon.
     """
 
-    def __init__(self, tokenizer: PreTrainedTokenizer, file_path: str, block_size: int):
+    def __init__(self, tokenizer: PreTrainedTokenizer, file_dir: str, block_size: int):
         warnings.warn(
             DEPRECATION_WARNING.format(
                 "https://github.com/huggingface/transformers/blob/master/examples/pytorch/language-modeling/run_mlm.py"
             ),
             FutureWarning,
         )
-        assert os.path.isfile(file_path), f"Input file path {file_path} not found"
+        # assert os.path.isfile(file_path), f"Input file path {file_path} not found"
         # Here, we do not cache the features, operating under the assumption
         # that we will soon use fast multithreaded tokenizers from the
         # `tokenizers` repo everywhere =)
-        logger.info(f"Creating features from dataset file at {file_path}")
+        logger.info("Creating features from dataset dir at %s", file_dir)
+        self.examples = []
+        import time
+        for file_name in os.listdir(file_dir):
+            file_path = os.path.join(file_dir, file_name)
+            print(file_path)
+            with open(file_path, encoding="utf-8") as f:
+                lines = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
 
-        with open(file_path, encoding="utf-8") as f:
-            lines = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
-
-        batch_encoding = tokenizer(lines, add_special_tokens=True, truncation=True, max_length=block_size)
-        self.examples = batch_encoding["input_ids"]
-        self.examples = [{"input_ids": torch.tensor(e, dtype=torch.long)} for e in self.examples]
+            batch_encoding = tokenizer(lines, add_special_tokens=True, truncation=True, max_length=block_size)
+            self.cur_examples = batch_encoding["input_ids"]
+            self.cur_examples = [{"input_ids": torch.tensor(e, dtype=torch.long)} for e in self.cur_examples]
+            self.examples.extend(self.cur_examples)
 
     def __len__(self):
         return len(self.examples)
